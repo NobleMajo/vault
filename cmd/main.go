04d556e1f0fd9c951e4065f3fa7547f4d07e5d94
NobleMajo <majo@coreunit.net>
2024-07-25 20:32:15 +0200

    Change: refactor main for new cryption package and use SafeWriteFileBytes

--- cmd/main.go
+++ cmd/main.go
@@ -7,12 +7,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
 
 	"coreunit.net/vault/cmd/config"
-	"coreunit.net/vault/internal/stringcrypt"
+	"coreunit.net/vault/internal/cryption"
 	"coreunit.net/vault/internal/stringfs"
 	"golang.org/x/term"
 )
@@ -101,306 +102,6 @@ func exitError(message string) {
 	os.Exit(1)
 }
 
-func initOperation(
-	targetFile string,
-	appConfig config.AppConfig,
-) {
-	targetVaultFile := targetFile + "." + appConfig.VaultFileExtension
-
-	exists := stringfs.Exists(targetFile + "." + appConfig.PlainFileExtension)
-	if exists {
-		exitError("File plain text file '" + targetFile + "." + appConfig.PlainFileExtension + "' already exists!")
-		return
-	}
-
-	exists = stringfs.Exists(targetFile + "." + appConfig.VaultFileExtension)
-	if exists {
-		exitError("File encrypted vault file '" + targetFile + "." + appConfig.VaultFileExtension + "' already exists!")
-		return
-	}
-
-	initText := "Hello and welcome to your own vault!\n\n<3"
-
-	publicKey, err := stringcrypt.LoadRsaPublicKey(appConfig.PublicKeyPath)
-
-	if err != nil {
-		exitError("Load public key error:\n> " + err.Error())
-		return
-	}
-
-	password, err := PromptNewPassword()
-
-	if err != nil {
-		exitError("Prompt new password error:\n> " + err.Error())
-		return
-	}
-
-	cipherPayload, err := VaultEncrypt(
-		initText,
-		appConfig.DoX509,
-		publicKey,
-		appConfig.DoAES256,
-		password,
-	)
-
-	if err != nil {
-		exitError("Vault encrypt error:\n> " + err.Error())
-		return
-	}
-
-	err = stringfs.SafeWriteFile(
-		targetVaultFile,
-		cipherPayload,
-		0644,
-	)
-
-	if err != nil {
-		exitError("Write file error:\n> " + err.Error())
-		return
-	}
-}
-
-func lockOperation(
-	targetFile string,
-	appConfig config.AppConfig,
-) {
-	sourcePlainFile := targetFile + "." + appConfig.PlainFileExtension
-	targetVaultFile := targetFile + "." + appConfig.VaultFileExtension
-
-	if _, err := os.Stat(sourcePlainFile); errors.Is(err, os.ErrNotExist) {
-		exitError("Plain source file '" + sourcePlainFile + "' does not exist!")
-		return
-	}
-
-	plainText, err := stringfs.ReadFile(sourcePlainFile)
-	if err != nil {
-		exitError("Read plain source error:\n> " + err.Error())
-		return
-	}
-
-	publicKey, err := stringcrypt.LoadRsaPublicKey(appConfig.PublicKeyPath)
-
-	if err != nil {
-		exitError("Load public key error:\n> " + err.Error())
-		return
-	}
-
-	password, err := PromptNewPassword()
-
-	if err != nil {
-		exitError("Prompt new password error:\n> " + err.Error())
-		return
-	}
-
-	cipherPayload, err := VaultEncrypt(
-		plainText,
-		appConfig.DoX509,
-		publicKey,
-		appConfig.DoAES256,
-		password,
-	)
-
-	if err != nil {
-		exitError("Vault encrypt error:\n> " + err.Error())
-		return
-	}
-
-	err = stringfs.SafeWriteFile(
-		targetVaultFile,
-		cipherPayload,
-		0640,
-	)
-
-	if err != nil {
-		exitError("Write file error:\n> " + err.Error())
-		return
-	}
-
-	fmt.Println("Locked!")
-}
-
-func unlockOperation(
-	targetFile string,
-	appConfig config.AppConfig,
-) {
-	sourceVaultFile := targetFile + "." + appConfig.VaultFileExtension
-	targetPlainFile := targetFile + "." + appConfig.PlainFileExtension
-
-	if _, err := os.Stat(sourceVaultFile); errors.Is(err, os.ErrNotExist) {
-		exitError("Source vault file '" + sourceVaultFile + "' does not exist!")
-		return
-	}
-
-	vaultRaw, err := stringfs.ReadFile(sourceVaultFile)
-
-	if err != nil {
-		exitError("Error while read vault source from '" + sourceVaultFile + "':\n> " + err.Error())
-		return
-	}
-
-	privateKey, err := stringcrypt.LoadRsaPrivateKey(appConfig.PrivateKeyPath)
-
-	if err != nil {
-		exitError("Load private key error:\n> " + err.Error())
-		return
-	}
-
-	password, err := PromptPassword()
-
-	if err != nil {
-		exitError("Prompt password error:\n> " + err.Error())
-		return
-	}
-
-	plainText, err := VaultDecrypt(
-		vaultRaw,
-		appConfig.DoX509,
-		privateKey,
-		appConfig.DoAES256,
-		password,
-	)
-
-	if err != nil {
-		exitError("Vault decrypt error:\n> " + err.Error())
-		return
-	}
-
-	err = stringfs.SafeWriteFile(
-		targetPlainFile,
-		plainText,
-		0640,
-	)
-
-	if err != nil {
-		exitError("Write file error:\n> " + err.Error())
-		return
-	}
-
-	err = stringfs.RemoveFile(sourceVaultFile)
-	if err != nil {
-		exitError("Remove source file error:\n> " + err.Error())
-		return
-	}
-
-	fmt.Println("Unlocked!")
-}
-
-func tempOperation(
-	targetFile string,
-	appConfig config.AppConfig,
-) {
-	unlockOperation(targetFile, appConfig)
-
-	sourceVaultFile := targetFile + "." + appConfig.VaultFileExtension
-	targetPlainFile := targetFile + "." + appConfig.PlainFileExtension
-
-	fmt.Println("Unlocked for 5 seconds!")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Lock vault now...")
-
-	plainText, err := stringfs.ReadFile(targetPlainFile)
-	if err != nil {
-		exitError("Read plain source error:\n> " + err.Error())
-		return
-	}
-
-	publicKey, err := stringcrypt.LoadRsaPublicKey(appConfig.PublicKeyPath)
-
-	if err != nil {
-		exitError("Load public key error:\n> " + err.Error())
-		return
-	}
-
-	password, err := PromptNewPassword()
-
-	if err != nil {
-		exitError("Prompt new password error:\n> " + err.Error())
-		return
-	}
-
-	encodedText, err := VaultEncrypt(
-		plainText,
-		appConfig.DoX509,
-		publicKey,
-		appConfig.DoAES256,
-		password,
-	)
-
-	if err != nil {
-		exitError("Vault encrypt error:\n> " + err.Error())
-		return
-	}
-
-	err = stringfs.SafeWriteFile(
-		sourceVaultFile,
-		encodedText,
-		0644,
-	)
-
-	if err != nil {
-		exitError("Write file error:\n> " + err.Error())
-		return
-	}
-
-	fmt.Println("Locked again!")
-}
-
-func printOperation(
-	targetFile string,
-	appConfig config.AppConfig,
-) {
-	sourceVaultFile := targetFile + "." + appConfig.VaultFileExtension
-
-	if _, err := os.Stat(sourceVaultFile); errors.Is(err, os.ErrNotExist) {
-		exitError("Source vault file '" + sourceVaultFile + "' does not exist!")
-		return
-	}
-
-	vaultRaw, err := stringfs.ReadFile(sourceVaultFile)
-
-	if err != nil {
-		exitError("Error while read vault source from '" + sourceVaultFile + "':\n> " + err.Error())
-		return
-	}
-
-	privateKey, err := stringcrypt.LoadRsaPrivateKey(appConfig.PrivateKeyPath)
-
-	if err != nil {
-		exitError("Load private key error:\n> " + err.Error())
-		return
-	}
-
-	password, err := PromptPassword()
-
-	if err != nil {
-		exitError("Prompt password error:\n> " + err.Error())
-		return
-	}
-
-	plainText, err := VaultDecrypt(
-		vaultRaw,
-		appConfig.DoX509,
-		privateKey,
-		appConfig.DoAES256,
-		password,
-	)
-
-	if err != nil {
-		exitError("Decrypt error:\n> " + err.Error())
-		return
-	}
-
-	if appConfig.CleanPrint {
-		fmt.Println(plainText)
-	} else {
-		fmt.Println(
-			"### Vault Content:\n\n" +
-				plainText + "\n\n" +
-				"### Don't forget to clear!",
-		)
-	}
-}
-
 func PromptNewPassword() (string, error) {
 	fmt.Println("Enter your new vault password:")
 	newPassword, err := ReadPassword()
@@ -432,28 +133,28 @@ func PromptPassword() (string, error) {
 }
 
 func VaultEncrypt(
-	payload string,
+	payload []byte,
 	doX509 bool,
 	X509PublicKey *rsa.PublicKey,
 	doAES256 bool,
-	AES256Key string,
-) (string, error) {
+	AES256Key []byte,
+) ([]byte, error) {
 	if !doX509 && !doAES256 {
-		return "", fmt.Errorf("no encryption method selected")
+		return nil, fmt.Errorf("no encryption method selected")
 	}
 	var err error
 
 	if doAES256 {
-		payload, err = stringcrypt.AES256Encrypt(AES256Key, payload)
+		payload, err = cryption.AES256Encrypt(AES256Key, payload)
 		if err != nil {
-			return "", fmt.Errorf("AES256 encrypt error, maybe wrong password:\n> %v", err)
+			return nil, fmt.Errorf("AES256 encrypt error, maybe wrong password:\n> %v", err)
 		}
 	}
 
 	if doX509 {
-		payload, err = stringcrypt.X509Encrypt(X509PublicKey, payload)
+		payload, err = cryption.X509AES256Encrypt(X509PublicKey, payload)
 		if err != nil {
-			return "", fmt.Errorf("x509 encrypt error:\n> %v", err)
+			return nil, fmt.Errorf("x509 encrypt error:\n> %v", err)
 		}
 	}
 
@@ -461,28 +162,28 @@ func VaultEncrypt(
 }
 
 func VaultDecrypt(
-	payload string,
+	payload []byte,
 	doX509 bool,
 	X509PrivateKey *rsa.PrivateKey,
 	doAES256 bool,
-	AES256Key string,
-) (string, error) {
+	AES256Key []byte,
+) ([]byte, error) {
 	if !doX509 && !doAES256 {
-		return "", fmt.Errorf("no decryption method selected")
+		return nil, fmt.Errorf("no decryption method selected")
 	}
 	var err error
 
 	if doX509 {
-		payload, err = stringcrypt.X509Decrypt(X509PrivateKey, payload)
+		payload, err = cryption.X509AES256Decrypt(X509PrivateKey, payload)
 		if err != nil {
-			return "", fmt.Errorf("x509 decrypt error:\n> %v", err)
+			return nil, fmt.Errorf("x509 decrypt error:\n> %v", err)
 		}
 	}
 
 	if doAES256 {
-		payload, err = stringcrypt.AES256Decrypt(AES256Key, payload)
+		payload, err = cryption.AES256Decrypt(AES256Key, payload)
 		if err != nil {
-			return "", fmt.Errorf("AES256 decrypt error, maybe wrong password:\n> %v", err)
+			return nil, fmt.Errorf("AES256 decrypt error, maybe wrong password:\n> %v", err)
 		}
 	}
 
@@ -528,4 +229,318 @@ func PrintHelp() {
 	)
 	flag.PrintDefaults()
 	fmt.Println()
+}
+
+func initOperation(
+	targetFile string,
+	appConfig config.AppConfig,
+) {
+	targetVaultFile := targetFile + "." + appConfig.VaultFileExtension
+
+	exists := stringfs.Exists(targetFile + "." + appConfig.PlainFileExtension)
+	if exists {
+		exitError("File plain text file '" + targetFile + "." + appConfig.PlainFileExtension + "' already exists!")
+		return
+	}
+
+	exists = stringfs.Exists(targetFile + "." + appConfig.VaultFileExtension)
+	if exists {
+		exitError("File encrypted vault file '" + targetFile + "." + appConfig.VaultFileExtension + "' already exists!")
+		return
+	}
+
+	initText := "Hello and welcome to your own vault!\n\n<3"
+
+	publicKey, err := cryption.LoadRsaPublicKey(appConfig.PublicKeyPath)
+
+	if err != nil {
+		exitError("Load public key error:\n> " + err.Error())
+		return
+	}
+
+	password, err := PromptNewPassword()
+
+	if err != nil {
+		exitError("Prompt new password error:\n> " + err.Error())
+		return
+	}
+
+	cipherPayload, err := VaultEncrypt(
+		[]byte(initText),
+		appConfig.DoX509,
+		publicKey,
+		appConfig.DoAES256,
+		[]byte(password),
+	)
+
+	if err != nil {
+		exitError("Vault encrypt error:\n> " + err.Error())
+		return
+	}
+
+	err = stringfs.SafeWriteFileBytes(
+		targetVaultFile,
+		cipherPayload,
+		0644,
+	)
+
+	if err != nil {
+		exitError("Write file error:\n> " + err.Error())
+		return
+	}
+}
+
+func lockOperation(
+	targetFile string,
+	appConfig config.AppConfig,
+) {
+	sourcePlainFile := targetFile + "." + appConfig.PlainFileExtension
+	targetVaultFile := targetFile + "." + appConfig.VaultFileExtension
+
+	if _, err := os.Stat(sourcePlainFile); errors.Is(err, os.ErrNotExist) {
+		exitError("Plain source file '" + sourcePlainFile + "' does not exist!")
+		return
+	}
+
+	plainText, err := stringfs.ReadFile(sourcePlainFile)
+	if err != nil {
+		exitError("Read plain source error:\n> " + err.Error())
+		return
+	}
+
+	publicKey, err := cryption.LoadRsaPublicKey(appConfig.PublicKeyPath)
+
+	if err != nil {
+		exitError("Load public key error:\n> " + err.Error())
+		return
+	}
+
+	password, err := PromptNewPassword()
+
+	if err != nil {
+		exitError("Prompt new password error:\n> " + err.Error())
+		return
+	}
+
+	cipherPayload, err := VaultEncrypt(
+		[]byte(plainText),
+		appConfig.DoX509,
+		publicKey,
+		appConfig.DoAES256,
+		[]byte(password),
+	)
+
+	if err != nil {
+		exitError("Vault encrypt error:\n> " + err.Error())
+		return
+	}
+
+	err = stringfs.SafeWriteFileBytes(
+		targetVaultFile,
+		cipherPayload,
+		0640,
+	)
+
+	if err != nil {
+		exitError("Write file error:\n> " + err.Error())
+		return
+	}
+
+	fmt.Println("Locked!")
+}
+
+func unlockOperation(
+	targetFile string,
+	appConfig config.AppConfig,
+) {
+	sourceVaultFile := targetFile + "." + appConfig.VaultFileExtension
+	targetPlainFile := targetFile + "." + appConfig.PlainFileExtension
+
+	if _, err := os.Stat(sourceVaultFile); errors.Is(err, os.ErrNotExist) {
+		exitError("Source vault file '" + sourceVaultFile + "' does not exist!")
+		return
+	}
+
+	vaultRaw, err := stringfs.ReadFile(sourceVaultFile)
+
+	if err != nil {
+		exitError("Error while read vault source from '" + sourceVaultFile + "':\n> " + err.Error())
+		return
+	}
+
+	privateKey, err := cryption.LoadRsaPrivateKey(appConfig.PrivateKeyPath)
+
+	if err != nil {
+		exitError("Load private key error:\n> " + err.Error())
+		return
+	}
+
+	password, err := PromptPassword()
+
+	if err != nil {
+		exitError("Prompt password error:\n> " + err.Error())
+		return
+	}
+
+	plainText, err := VaultDecrypt(
+		[]byte(vaultRaw),
+		appConfig.DoX509,
+		privateKey,
+		appConfig.DoAES256,
+		[]byte(password),
+	)
+
+	if err != nil {
+		exitError("Vault decrypt error:\n> " + err.Error())
+		return
+	}
+
+	err = stringfs.SafeWriteFileBytes(
+		targetPlainFile,
+		plainText,
+		0640,
+	)
+
+	if err != nil {
+		exitError("Write file error:\n> " + err.Error())
+		return
+	}
+
+	err = stringfs.RemoveFile(sourceVaultFile)
+	if err != nil {
+		exitError("Remove source file error:\n> " + err.Error())
+		return
+	}
+
+	fmt.Println("Unlocked!")
+}
+
+func tempOperation(
+	targetFile string,
+	appConfig config.AppConfig,
+) {
+	var err error
+	decodeTime := 10
+
+	if len(appConfig.Args) == 3 && len(appConfig.Args[2]) != 0 {
+		rawDecodeTime := appConfig.Args[2]
+
+		decodeTime, err = strconv.Atoi(strings.TrimSpace(rawDecodeTime))
+
+		if err != nil {
+			exitError("Error parsing decode time to number '" + rawDecodeTime + "':\n> " + err.Error())
+			return
+		}
+	}
+
+	unlockOperation(targetFile, appConfig)
+
+	sourceVaultFile := targetFile + "." + appConfig.VaultFileExtension
+	targetPlainFile := targetFile + "." + appConfig.PlainFileExtension
+
+	fmt.Println("Unlocked for " + strconv.Itoa(decodeTime) + " seconds!")
+	time.Sleep(time.Duration(decodeTime) * time.Second)
+	fmt.Println("Lock vault now...")
+
+	plainText, err := stringfs.ReadFile(targetPlainFile)
+	if err != nil {
+		exitError("Read plain source error:\n> " + err.Error())
+		return
+	}
+
+	publicKey, err := cryption.LoadRsaPublicKey(appConfig.PublicKeyPath)
+
+	if err != nil {
+		exitError("Load public key error:\n> " + err.Error())
+		return
+	}
+
+	password, err := PromptNewPassword()
+
+	if err != nil {
+		exitError("Prompt new password error:\n> " + err.Error())
+		return
+	}
+
+	encodedText, err := VaultEncrypt(
+		[]byte(plainText),
+		appConfig.DoX509,
+		publicKey,
+		appConfig.DoAES256,
+		[]byte(password),
+	)
+
+	if err != nil {
+		exitError("Vault encrypt error:\n> " + err.Error())
+		return
+	}
+
+	err = stringfs.SafeWriteFileBytes(
+		sourceVaultFile,
+		encodedText,
+		0644,
+	)
+
+	if err != nil {
+		exitError("Write file error:\n> " + err.Error())
+		return
+	}
+
+	fmt.Println("Locked again!")
+}
+
+func printOperation(
+	targetFile string,
+	appConfig config.AppConfig,
+) {
+	sourceVaultFile := targetFile + "." + appConfig.VaultFileExtension
+
+	if _, err := os.Stat(sourceVaultFile); errors.Is(err, os.ErrNotExist) {
+		exitError("Source vault file '" + sourceVaultFile + "' does not exist!")
+		return
+	}
+
+	vaultRaw, err := stringfs.ReadFile(sourceVaultFile)
+
+	if err != nil {
+		exitError("Error while read vault source from '" + sourceVaultFile + "':\n> " + err.Error())
+		return
+	}
+
+	privateKey, err := cryption.LoadRsaPrivateKey(appConfig.PrivateKeyPath)
+
+	if err != nil {
+		exitError("Load private key error:\n> " + err.Error())
+		return
+	}
+
+	password, err := PromptPassword()
+
+	if err != nil {
+		exitError("Prompt password error:\n> " + err.Error())
+		return
+	}
+
+	plainText, err := VaultDecrypt(
+		[]byte(vaultRaw),
+		appConfig.DoX509,
+		privateKey,
+		appConfig.DoAES256,
+		[]byte(password),
+	)
+
+	if err != nil {
+		exitError("Decrypt error:\n> " + err.Error())
+		return
+	}
+
+	if appConfig.CleanPrint {
+		fmt.Println(plainText)
+	} else {
+		fmt.Println(
+			"### Vault Content:\n\n" +
+				string(plainText) + "\n\n" +
+				"### Don't forget to clear!",
+		)
+	}
 }
